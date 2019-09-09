972a0f3e4c0ba5561d67f6744fd6d7e5d7f601be
Emily-monce <54906186+Emily-monce@users.noreply.github.com>
2019-09-09 12:03:33 -0400

    Add files via upload

---
+++ classwork.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class classwork {
+	public static void main(String[] args) {
+		
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter three int values");
+		int num1 = input.nextInt();
+		int num2 = input.nextInt();
+		int num3 = input.nextInt();
+		int result = num1 + num2 + num3;
+	System.out.print(result);
+		
+		
+		
+		
+	}
+
+}
---
+++ multiply.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class multiply {
+	public static void main(String[] args) { 
+	
+	Scanner casa = new Scanner(System.in);
+	System.out.println("Enter three values");
+		int h1 = casa.nextInt();
+		int h2 = casa.nextInt();
+		int h3 = casa.nextInt();
+		int h4 = casa.nextInt();
+		int result = h1 * h2 * h3* h4;
+	System.out.print(result);
+	
+		
+		
+		
+	}
+}
