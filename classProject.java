b0bbd718c4b653041de76feb4fbee90400963e35
Emily-monce <54906186+Emily-monce@users.noreply.github.com>
2019-10-02 12:38:40 -0400

    Add files via upload

---
+++ classProject.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class classProject {
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+		int numTotal;
+		
+		Scanner num = new Scanner (System.in);
+		System.out.println("Enter Number:");
+		num1 = num.nextInt();
+		System.out.println("Enter Second Number:");
+		num2 = num.nextInt();
+		numTotal = num1 + num2;
+		System.out.println(numTotal);
+		
+	}
+}
