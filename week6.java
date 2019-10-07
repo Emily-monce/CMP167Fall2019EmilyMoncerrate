35fa1cf159be8c8fd26476e967b91945a8842451
Emily-monce <54906186+Emily-monce@users.noreply.github.com>
2019-10-07 12:40:22 -0400

    Add files via upload

---
+++ week6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class week6 {
+	public static void main(String[] args) {
+		
+		System.out.println(productofThree(5, 7, 9));
+		
+		
+		
+	}
+	public static int productofThree(int x, int y, int z) {
+		return x * y * z;
+		
+		
+	}
+
+}
